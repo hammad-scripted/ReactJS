d962a5b6b5a1470998b62fecddb24e03a7519100
Mohammad Hammad <hammad.scripted@gmail.com>
2026-05-23 23:24:48 +0530

    Add initial setup for Vite project with React and Tailwind CSS
    - Created main entry point (main.jsx) for React application
    - Added Tailwind CSS import in index.css
    - Configured Vite with Tailwind CSS plugin in vite.config.js
    - Added React and Vite logos as SVG assets

--- 02_Tailwind/tailwind-setup/src/App.jsx
+++ 02_Tailwind/tailwind-setup/src/App.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import User from './User';
 import Card from './Card';
+import Hero from './Hero';
 const App = () => {
   return (
-    <div
-      className="min-h-screen bg-gray-800 flex
-      flex-col items-center justify-center"
-    >
-      <h1 className="text-4xl font-bold text-white mb-8">
-        Hello, Tailwind CSS!
-      </h1>
+    <Hero />
+    // <div
+    //   className="min-h-screen bg-gray-800 flex
+    //   flex-col items-center justify-center"
+    // >
+    //   <h1 className="text-4xl font-bold text-white mb-8 border-b-4 border-blue-500">
+    //     Learning Tailwind CSS with React
+    //   </h1>
 
-      <Card
-        title="Card Title"
-        description="This is a description of the card content."
-      />
-      <Card
-        title="Another Card"
-        description="This is another card with different content."
-      />
-    </div>
+    //   <Card
+    //     title="Card Title"
+    //     description="This is a description of the card content."
+    //     src="https://images.unsplash.com/photo-1777047023536-8e47688b77f9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //   />
+    //   <Card
+    //     title="Another Card"
+    //     description="This is another card with different content."
+    //     src="https://images.unsplash.com/photo-1772289935758-d4190f9f849d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //   />
+    // </div>
   );
 };
 
--- 02_Tailwind/tailwind-setup/src/Card.jsx
+++ 02_Tailwind/tailwind-setup/src/Card.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Card = (props) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 my-6">
-      <img src="path/to/image.jpg" alt="Description" />
+    <div
+      className="bg-white rounded-lg shadow-md p-6 my-6 w-80 transition-transform transform hover:scale-104 duration-300 shadow-gray-400/50 hover:shadow-gray-400/80 
+      hover:shadow-lg
+
+    "
+    >
+      <img
+        src={props.src}
+        alt="Description"
+        className="w-full h-48 object-cover rounded-md mb-4"
+      />
       <h2 className="text-2xl font-bold">{props.title}</h2>
       <p className="text-gray-600">{props.description}</p>
 
